c0609233c192fee4df4e055cfaf00ee4e0a3d6e5
Audrey-Ella-xo <anitaudrey@gmail.com>
2019-11-12 16:00:52 +0100

    fixed board update

--- bin/main.rb
+++ bin/main.rb
@@ -10,28 +10,28 @@ def show_board(
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-  
-    # puts show_board([1,2,3,4,5,6,7,8,9])
-    puts "Welcome to the game! who will you be today? X or O?"
-    player1 = gets.chomp
-    player1.upcase!
-    
-    if player1 == "X"
-        player2 = "O"
-        puts "player 1 is #{player1} and player 2 is #{player2}."
 
-    else
-        player2 = "X"
-        puts "player 1 is #{player1} and player 2 is #{player2}."
-    end
+board = ["1 ", "2 ", " 3", "4 ", "5 ", "6 ", " 7", " 8", " 9"]
+#puts show_board([1,2,3,4,5,6,7,8,9])
+puts "Welcome to the game! who will you be today? X or O?"
+player1 = gets.chomp
+player1.upcase!
 
-    puts "In what numbered cell would you start from?"
-    input = gets.chomp
-    input.upcase!
-    show_board.map do |x|
-        if show_board.include? input
-            x = input
-        end
-        puts show_board
-    end
+if player1 == "X"
+    player2 = "O"
+    puts "player 1 is #{player1} and player 2 is #{player2}."
+
+else
+    player2 = "X"
+    puts "player 1 is #{player1} and player 2 is #{player2}."
+end
+
+puts "In what numbered cell would you start from?"
+input = gets.chomp
+input.upcase!
+turn = Integer(input)
+board[turn-1] = player1
+puts `clear`
+
+show_board(board)
     
