d30388cb18449a34262e9f525e2f0c41e8c6915b
Audrey-Ella-xo <anitaudrey@gmail.com>
2019-11-12 15:11:37 +0100

    added some functionality to the showboard

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,37 @@
 #!/usr/bin/env ruby
 puts "Hello world!"
+def show_board(
+    board = ["1 ", "2 ", " 3", "4 ", "5 ", "6 ", " 7", " 8", " 9"])
+      puts 'show board'
+    #board output
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+  
+    # puts show_board([1,2,3,4,5,6,7,8,9])
+    puts "Welcome to the game! who will you be today? X or O?"
+    player1 = gets.chomp
+    player1.upcase!
+    
+    if player1 == "X"
+        player2 = "O"
+        puts "player 1 is #{player1} and player 2 is #{player2}."
+
+    else
+        player2 = "X"
+        puts "player 1 is #{player1} and player 2 is #{player2}."
+    end
+
+    puts "In what numbered cell would you start from?"
+    input = gets.chomp
+    input.upcase!
+    show_board.map do |x|
+        if show_board.include? input
+            x = input
+        end
+        puts show_board
+    end
+    
