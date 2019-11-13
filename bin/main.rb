ab90ccb53ed7a22544d787b4deaac80545a5b4b2
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-13 12:25:38 +0100

    completed the game logic

--- bin/main.rb
+++ bin/main.rb
@@ -2,134 +2,98 @@
 # frozen_string_literal: true
 # rubocop:disable all
 puts 'Hello world!'
-def show_board(
-  board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-)
-  puts 'show board'
-  # board output
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts '-----------'
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts '-----------'
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
-board = ['1 ', '2 ', ' 3', '4 ', '5 ', '6 ', ' 7', ' 8', ' 9']
-show_board(board)
-puts 'Welcome to the game! who will you be today? X or O?'
-player1 = gets.chomp
-player1.upcase!
+require_relative '../lib/manager.rb'
+require_relative '../lib/player.rb'
 
-if player1 == 'X'
-  player2 = 'O'
-  puts "player 1 is #{player1} and player 2 is #{player2}."
+class UserInterface
+  def initialize
+    @game = GameManager.new(@players)
+  end
 
-else
-  player2 = 'X'
-  puts "player 1 is #{player1} and player 2 is #{player2}."
-end
+  #instructions to player
+  def welcome
+    puts ' '
+    puts '==========TIC-TAC-TOE=========='
+    puts 'Welcome to tic tac toe'
+    puts '-------------INSTRUCTIONS---------------'
+    puts 'The first player is X'
+    puts 'Choose numbers from 1 to 9 to select desired cell'
+    puts 'No duplicate numbers are allowed'
+    puts '--------------------------------'
+    # Request for player names and save them to p1 and p2
+    puts 'Player 1 please enter your name: '
+    @player_one = gets.chomp
 
-puts 'In what numbered cell would you start from?'
-input = gets.chomp
-input.upcase!
-turn = Integer(input)
-board[turn - 1] = player1
+    puts 'Player 2 please enter your name: '
+    @player_two = gets.chomp
 
-show_board(board)
+    puts "Player one X is #{@player_one} vs Player two O is #{@player_two}"
+    p "Here's the board"
+    display_board
+  end
 
-puts 'Now, your move is displayed on the board'
+  def end
+      puts 'Thank you for playing Tic Tac Toe'
+  end
 
-def input
-  puts "If Player 1 enters 'X' then automatically 'O' is assigned to Player 2"
-  puts 'Display the board'
-  puts "Any cell that player 1 chooses, player 2's input shouldn't override that of player 1"
-  puts 'The show_board method should be updated and shown on the screen'
-end
+  #Tic Tac Toe Board
+  def display_board
+    puts " #{@game.board[0]} | #{@game.board[1]} | #{@game.board[2]} "
 
-def player_turn
-  puts 'Whenever Player 1 chooses a cell, player 2 should be alerted to play'
-  puts 'The player who is to play next should select from available moves.'
-  puts 'The game should informs player if selected move is invalid.'
-  puts 'To check if the game has come to an end'
-end
+    puts ' ---------- '
 
-def win?
-  puts 'if one of the players enters the required combination, the draw method should be false and a message should be shown indicating the player who won'
-end
+    puts " #{@game.board[3]} | #{@game.board[4]} | #{@game.board[5]} "
 
-def move(board, index, char)
-  board[index] = char
-end
+    puts ' ---------- '
 
-def position_taken?(board, index)
-  !(board[index] == ' ' || board[index].nil?)
-end
+    puts " #{@game.board[6]} | #{@game.board[7]} | #{@game.board[8]} "
+  end
 
-def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken(board, index)
-    puts 'This is a valid move' if char >= 1 && char <= 9 while char < 9
-    true
-  else
-    puts 'this move is invalid!'
-    false
+  def position
+    current = @game.current_player
+    if current == :X
+      print "\n #{@player_one}, choose a position between 1-9: "
+    else
+      print "\n #{@player_two}, choose a position between 1-9: "
+    end
+  end
+
+    # Game begins, player 1 starts choosing between 1-9
+  # if the input is different valid_move returns false and input is request again
+  def turn
+    position
+    spot = gets.chomp
+    spot = @game.board_index(spot)
+    if @game.valid_move?(spot)
+      @game.set_input(spot, @game.current_player)
+      display_board
+    else
+      puts 'This spot has been taken or you inputed a wrong value'
+      puts 'Please enter a valid option between 1-9'
+      display_board
+      turn
+    end
+  end
+
+  # Turn method is called for players to fill the board until @game.over? method is true
+  # If total moves = 9 and no combinations are detected, its a draw, if a combination is detected, @game.won is executed
+  def play
+    turn until @game.over?
+      if @game.won?
+        if @game.winner? == :X
+        puts "CONGRATULATIONS #{@player_one}! You Won"
+        else
+          puts "CONGRATULATIONS #{@player_two}! You Won"
+        end
+      elsif @game.draw?
+        puts "It's a draw!"
+      end
   end
 end
 
-def turn_game
-  puts 'Kindly select a cell between 1-9 apart from your first selection: '
+game = UserInterface.new
+game.welcome
+game.play
+game.end
 
-  input = gets.chomp
-  input.upcase!
-  turn = Integer(input)
-  board[turn - 1] = player1
-
-  if valid_move?(board, index)
-    puts 'Valid move'
-    move(board, index, char)
-    show_board(board)
-  else
-    puts 'Choose a valid position! '
-    turn_game(board)
-  end
-  show_board(board)
-  puts 'Now, your move is displayed on the board'
-end
-
-def turn_count(board)
-  board.count { |char| char == 'X' || char == 'O' }
-end
-
-# rubocop:enable all
-def current_player
-  current_player = if turn_count(board).even?
-                     'X'
-                   else
-                     'O'
-                   end
-  current_player
-end
-
-def full?(board)
-  if board.include?(' ') || board.include?('')
-    false
-  else
-    true
-  end
-end
-# rubocop:disable all
-def draw?(board)
-  return true if !won?(board) && full?(board)
-
-  puts 'if all the cells are filled up without the expected winning combinations, then a message should be printed showing the draw/tie'
-end
-
-def over?(board)
-  puts 'is it over?'
-  if won?(board) || draw?(board) || full?(board)
-    return true
-  else
-    puts 'no keep going'
-    return false
-  end
-end
-# rubocop:enable all
---
+++ lib/manager.rb
@@ -0,0 +1,78 @@
+require_relative './player.rb'
+
+class GameManager
+  attr_reader :board, :player_one, :player_two, :X, :O
+  def initialize(players, board = (1..9).to_a)
+    @board = board
+    @player_one = Player.new(players)
+    @player_two = Player.new(players)
+  end
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [6, 4, 2],
+    [0, 4, 8]
+  ].freeze
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+        @board[combo[1]] == @board[combo[2]] &&
+        space_filled?(combo[0])
+    end
+  end
+
+  def set_input(index, input = :X)
+    @board[index] = input
+  end
+
+  def space_filled?(index)
+    @board[index] == :X || @board[index] == :O
+  end
+
+  def board_index(input_index)
+    input_index.to_i - 1
+  end
+
+  def valid_move?(position)
+    position.between?(0, 8) && !space_filled?(position)
+  end
+
+  def turn_count
+    @board.select { |e| e == :X || e == :O }.size # rubocop:disable Style/MultipleComparison
+  end
+
+  def current_player
+    player = if (turn_count % 2).zero?
+               :X
+             else
+               :O
+             end
+    player
+  end
+
+  def full?
+    turn_count == 9
+  end
+
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || full? || draw?
+  end
+
+  def winner?
+    if @board[won?.first] == :X
+      :X
+    else
+      :O
+    end
+  end
+end
