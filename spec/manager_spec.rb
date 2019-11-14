8406c3c2d1631dbb9c210da22d5185f2be6b1e8b
Audrey-Ella-xo <anitaudrey@gmail.com>
2019-11-14 19:35:32 +0100

    finished set_input test and valid move test

--- spec/manager_spec.rb
+++ spec/manager_spec.rb
@@ -10,34 +10,19 @@ describe GameManager do
     end
   end
 
-#   describe '#valid_move?' do
+  describe '#set_input' do
+    let(:inp) { GameManager.new([Player.new('Fernando')])}
+    it 'inserts player input to a cell on the board' do
+      board = (1..9).to_a
+      expect(inp.set_input(board[1])).to eql(:X)
+    end
+  end
   
-# end
+  describe '#valid_move?' do
+    let(:pos) { GameManager.new([Player.new('Fernando')])}
+    it 'checks if the position on the board is valid' do
+      expect(pos.valid_move?(3)).to eql(true)
+    end
+  end
 
-describe '#set_input' do
-let(:board) { GameManager.new(board[1])}
-it 'inserts X into a cell' do
-  board = 1..9
-  expect(board[0], "X").to eql(board[3], "X")
 end
-end
-end
-
-# before {@valid_move = Player.new("abc", "X").make_move?("#{pos}", Board.new)}
-
-#     describe "#move_valid?" do
-#         context "3 on the board" do
-#             let(:pos) {"3"}
-#             it "3 is a valid spot on the board" do
-#                 expect(@valid_move).to eql(true)
-#             end
-#         end
-
-#         def move_valid?(move)
-#           correct_pos = ("1".."9").include?(move)
-#           if correct_pos
-#               is_not_occupied = pos[move.to_i - 1].eql?(move)
-#           end
-#           correct_pos and is_not_occupied
-#       end
-  
